b5e221294be8027b501bf8e81e86e6c49e8392d9
Alejo2075 <alejsant75@gmail.com>
2024-08-28 11:08:28 -0500

    Initial commit

---
+++ src/main/java/org/alejo2075/algorithmic_trading_platform/AlgorithmicTradingPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.alejo2075.algorithmic_trading_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmicTradingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmicTradingPlatformApplication.class, args);
+	}
+
+}
